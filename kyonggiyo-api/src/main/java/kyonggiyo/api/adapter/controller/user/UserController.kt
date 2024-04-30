dd137b8a9c5b61e38e09bf6d082446e0bcaa31ae
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-05-01 01:15:18 +0900

    :recycle: Refactor: User 컨트롤러 코틀린 리팩토링

---
+++ kyonggiyo-api/src/main/java/kyonggiyo/api/adapter/controller/user/UserController.kt
@@ -0,0 +1,41 @@
+package kyonggiyo.api.adapter.controller.user
+
+import kyonggiyo.api.adapter.controller.user.dto.UserCreateRequest
+import kyonggiyo.api.adapter.controller.user.dto.UserDeleteRequest
+import kyonggiyo.api.adapter.controller.user.dto.ValidateNicknameRequest
+import kyonggiyo.api.adapter.controller.user.dto.ValidateNicknameResponse
+import kyonggiyo.application.port.`in`.auth.ProvideAuthCodeUrlUseCase
+import kyonggiyo.application.port.`in`.user.CreateUserUseCase
+import kyonggiyo.application.port.`in`.user.ValidateNicknameUseCase
+import kyonggiyo.application.port.`in`.user.WithdrawUserUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/users/")
+class UserController(
+    private val createUserUseCase: CreateUserUseCase,
+    private val provideAuthCodeUrlUseCase: ProvideAuthCodeUrlUseCase,
+    private val validateNicknameUseCase: ValidateNicknameUseCase,
+    private val withdrawUserUseCase: WithdrawUserUseCase,
+) {
+
+    @PostMapping("/profile")
+    fun userCreate(@RequestBody request: UserCreateRequest): ResponseEntity<Unit> {
+        return createUserUseCase.createUser(request.toCommand())
+            .let { provideAuthCodeUrlUseCase.provideUri(it) }
+            .let { ResponseEntity.created(it).build() }
+    }
+
+    @GetMapping("/profile/nickname")
+    fun nicknameValidate(request: ValidateNicknameRequest): ResponseEntity<ValidateNicknameResponse> {
+        return validateNicknameUseCase.existByNickname(request.nickname)
+            .let { ResponseEntity.ok(ValidateNicknameResponse(it)) }
+    }
+
+    @DeleteMapping("/profile/withdraw")
+    fun withdraw(@RequestBody request: UserDeleteRequest): ResponseEntity<Unit> {
+        withdrawUserUseCase.deleteUser(request.toCommand())
+        return ResponseEntity.noContent().build()
+    }
+}
