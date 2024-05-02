0eea72e18de4e1f5db04ae8cdb9378c415f603a1
weonest <geonhee33@naver.com>
2024-05-02 14:41:56 +0900

    :recycle: Account Kotlin 코드로 수정 + User와 연관관계 제거

--- kyonggiyo-core/src/main/java/kyonggiyo/domain/auth/Account.kt
+++ kyonggiyo-core/src/main/java/kyonggiyo/domain/auth/Account.kt
@@ -1,63 +1,44 @@
-package kyonggiyo.domain.auth;
+package kyonggiyo.domain.auth
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import kyonggiyo.domain.BaseEntity;
-import kyonggiyo.domain.auth.exception.AccountErrorCode;
-import kyonggiyo.domain.user.User;
-import kyonggiyo.common.exception.InvalidStateException;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*
+import kyonggiyo.common.exception.InvalidStateException
+import kyonggiyo.domain.BaseEntity
+import kyonggiyo.domain.auth.exception.AccountErrorCode
 
-@Getter
 @Entity
-@EqualsAndHashCode
 @Table(name = "accounts")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Account extends BaseEntity {
+class Account(
+        platform: Platform,
+        platformId: String
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    val id: Long? = null
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private Platform platform;
+    var platform: Platform = platform
+        protected set
 
-    private String platformId;
+    @Column(nullable = false)
+    var platformId: String = platformId
+        protected set
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(nullable = true)
+    var userId: Long? = null
+        protected set
 
-    public Account(Platform platform, String platformId) {
-        this.platform = platform;
-        this.platformId = platformId;
+    fun hasNoUser(): Boolean {
+        return this.userId == null
     }
 
-    public boolean hasNoUser() {
-        return this.user == null;
-    }
-
-    public void registerUser(User user) {
-        if (this.user == null) {
-            this.user = user;
-            return;
+    fun registerUser(userId: Long) {
+        if (this.userId == null) {
+            this.userId = userId
+            return
         }
-        throw new InvalidStateException(AccountErrorCode.ALREADY_HAS_USER);
-    }
-
-    public Long getUserId() {
-        return user.getId();
+        throw InvalidStateException(AccountErrorCode.ALREADY_HAS_USER);
     }
 
 }
