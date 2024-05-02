7024e273006d12aec58835e4ad1a8e93206b45ab
weonest <geonhee33@naver.com>
2024-05-02 14:42:35 +0900

    :recycle: AccountLoginService Kotlin 코드로 변경

--- kyonggiyo-core/src/main/java/kyonggiyo/application/service/auth/AccountLoginService.kt
+++ kyonggiyo-core/src/main/java/kyonggiyo/application/service/auth/AccountLoginService.kt
@@ -1,23 +1,21 @@
-package kyonggiyo.application.service.auth;
+package kyonggiyo.application.service.auth
 
-import kyonggiyo.application.port.out.auth.LoadAccountPort;
-import kyonggiyo.domain.auth.Account;
-import kyonggiyo.domain.auth.Platform;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import kyonggiyo.application.port.out.auth.LoadAccountPort
+import kyonggiyo.domain.auth.Account
+import kyonggiyo.domain.auth.Platform
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-@RequiredArgsConstructor
-public class AccountLoginService {
+class AccountLoginService(
+    private val loadAccountPort: LoadAccountPort,
+    private val accountSignUpService: AccountSignUpService
+) {
 
-    private final LoadAccountPort loadAccountPort;
-    private final AccountSignUpService accountSignUpService;
-    
     @Transactional(readOnly = true)
-    public Account login(Platform platform, String platformId) {
+    fun login(platform: Platform, platformId: String): Account {
         return loadAccountPort.findByPlatformAndPlatformId(platform, platformId)
-                .orElseGet(() -> accountSignUpService.signup(platform, platformId));
+                .let { accountSignUpService.signup(platform, platformId) }
     }
 
 }
